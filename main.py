943f962bf382c9e9c2d740386d2b5951f6ada19c
Abdugaffor <75927183+aberrra@users.noreply.github.com>
2023-05-12 15:06:41 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -11,16 +11,10 @@ postgres_port = os.environ.get("POSTGRES_PORT")
 postgres_db = os.environ.get("POSTGRES_DB")
 postgres_user = os.environ.get("POSTGRES_USER")
 postgres_password = os.environ.get("POSTGRES_PASSWORD")
-mongo_host = os.environ.get("MONGO_HOST")
-mongo_db = os.environ.get("MONGO_DB")
-mongo_user = os.environ.get("MONGO_USER")
-mongo_password = os.environ.get("MONGO_PORT")
-mongo_port = os.environ.get("MONGO_PASSWORD")
 token = os.environ.get("TOKEN"),
 address = os.environ.get("ADDRESS")
 pod = os.environ.get("MY_POD_NAME")
 node = os.environ.get("MY_NODE_NAME")
-ns = os.environ.get("MY_POD_NAMESPACE")
 
 @app.route('/', methods=['GET'])
 def default():
@@ -35,7 +29,7 @@ def default():
     return jsonify(res)
 
 @app.route('/psql', methods=['GET'])
-def get_psql():
+def get_project():
     res = {
 		'string': 'PSQL',
         'postgres': postgres,
@@ -43,18 +37,6 @@ def get_psql():
         'postgres_user': postgres_user,
         'postgres_db': postgres_db,
         'postgres_password': postgres_password,
-	}
-    return jsonify(res)
-
-@app.route('/mongo', methods=['GET'])
-def get_mongo():
-    res = {
-		'string': 'Mongo',
-        'mongo': mongo_host,
-        'mongo_port': mongo_port,
-        'mongo_user': mongo_user,
-        'mongo_db': mongo_db,
-        'mongo_password': mongo_password,
 	}
     return jsonify(res)
 
@@ -70,9 +52,8 @@ def get_address():
 @app.route('/pod', methods=['GET'])
 def get_pod():
     res = {
-		'string': 'Pod',
+		'string': 'Payme',
         'pod': pod,
-        'node': node,
-        'namespace': ns
+        'node': node
 	}
     return jsonify(res)
